1f2547d33e6dd151d1e608439dd589eeff629c57
nernoel <142674871+nernoel@users.noreply.github.com.>
2024-06-04 19:34:13 -0400

    updates made

---
+++ src/app/components/CreatePost/index.ts
@@ -0,0 +1,39 @@
+"use server"
+
+import { PrismaClient } from '@prisma/client';
+import {auth} from '@/app/api/auth/[...nextauth]/auth'
+
+interface Post {
+    title: string;
+    description: string;
+    location: string;
+    status: string;
+    // createdAt: Date;
+    }
+
+export const postData = async ({ title, description, location, status}: Post) => {
+    const prisma = new PrismaClient();
+    const session = await auth();
+    
+    try {
+        const postData = await prisma.post.create({
+            data: {
+                title: title,
+                description: description,
+                location: location,
+                status: status,
+                userEmail: session?.user?.email!
+                
+               
+            }
+        });
+        return postData;
+
+    } catch (error) {
+        console.error("Error creating post:", error);
+        throw error;
+
+    } finally {
+        await prisma.$disconnect();
+    }
+};
